aa7666d2dd09acbf7bec05193b8845e515ba35b4
Megha <meghaa.2703@gmail.com>
2020-07-02 00:31:35 +0530

    Remove duplicate files

--- djangotest/boards/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from boards.models import Board, Post, Topic
-# Register your models here.
-
-admin.site.register(Board)
-admin.site.register(Post)
-admin.site.register(Topic)
